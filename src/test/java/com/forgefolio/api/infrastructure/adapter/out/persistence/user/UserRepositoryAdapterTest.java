d6b92bc66c902ae7e974f26c9b9ff70b952009aa
kaue-soares <kaue.felipe@3035tech.com>
2025-07-22 09:14:28 -0300

    feat: test for user not found on repository

--- src/test/java/com/forgefolio/api/infrastructure/adapter/out/persistence/user/UserRepositoryAdapterTest.java
+++ src/test/java/com/forgefolio/api/infrastructure/adapter/out/persistence/user/UserRepositoryAdapterTest.java
@@ -1,16 +1,21 @@
 package com.forgefolio.api.infrastructure.adapter.out.persistence.user;
 
 import com.forgefolio.api.application.port.out.persistence.PersistenceContextExecutor;
+import com.forgefolio.api.domain.exception.ErrorCode;
+import com.forgefolio.api.domain.exception.NotFoundException;
+import com.forgefolio.api.domain.model.shared.Id;
 import com.forgefolio.api.domain.model.user.User;
 import com.forgefolio.api.infrastructure.adapter.out.persistence.IntegrationTestcontainersManager;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.vertx.RunOnVertxContext;
 import io.quarkus.test.vertx.UniAsserter;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @QuarkusTest
@@ -26,6 +31,7 @@ class UserRepositoryAdapterTest {
     }
 
     @Test
+    @DisplayName("When saving a user, should persist the user and be able to find it by ID")
     @RunOnVertxContext
     void save(UniAsserter asserter) {
         User user = new User();
@@ -48,6 +54,7 @@ class UserRepositoryAdapterTest {
     }
 
     @Test
+    @DisplayName("When using a valid user ID, should find the user by ID")
     @RunOnVertxContext
     void findById(UniAsserter asserter) {
         User user = new User();
@@ -65,6 +72,27 @@ class UserRepositoryAdapterTest {
                 found -> {
                     assertNotNull(found, "User should not be null after saving");
                     assertEquals(user.getId(), found.getId(), "User ID should match after saving");
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("When using an invalid user ID, should throw an NotFoundException with ErrorCode.USER_NOT_FOUND")
+    @RunOnVertxContext
+    void findByIdNotFound(UniAsserter asserter) {
+        Id invalidId = new Id(UUID.randomUUID());
+
+        asserter.assertFailedWith(
+                () -> persistenceContextExecutor.runInSession(
+                        () -> userRepositoryAdapter.findById(invalidId)
+                ),
+                (e) -> {
+                    assertNotNull(e, "Exception should not be null");
+                    assertInstanceOf(NotFoundException.class, e, "Should throw NotFoundException");
+
+                    NotFoundException notFoundException = (NotFoundException) e;
+
+                    assertEquals(ErrorCode.USER_NOT_FOUND, notFoundException.getErrorCode(), "Error code should be ErrorCode.USER_NOT_FOUND");
                 }
         );
     }
